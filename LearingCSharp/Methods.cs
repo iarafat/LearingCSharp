37d6041355195b29a7eab126c972475d56b93971
iarafat <miyasinarafat@gmail.com>
2019-03-26 12:32:18 +0600

    updating the method works

--- LearingCSharp/Methods.cs
+++ LearingCSharp/Methods.cs
@@ -10,21 +10,34 @@ namespace LearingCSharp
     {
         static void Main(string[] args)
         {
-            ReverseString();
+            Console.WriteLine("The Name Game");
 
+            Console.Write("What's you first name? ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What's you last name? ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("In hat city were you born? ");
+            string city = Console.ReadLine();
+
+            Console.Write("Results: ");
+            ReverseString(firstName);
+            ReverseString(lastName);
+            ReverseString(city);
 
             Console.ReadLine();
         }
 
-        private static void ReverseString()
+        private static void ReverseString(string message)
         {
-            string message = "Hello world!";
             char[] messageArray = message.ToCharArray();
             Array.Reverse(messageArray);
             foreach (var item in messageArray)
             {
                 Console.Write(item);
             }
+            Console.Write(" ");
         }
     }
 }
