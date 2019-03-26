2e92b437a6e8d0cca13f8639d9b49a798a785bcb
iarafat <miyasinarafat@gmail.com>
2019-03-26 12:16:57 +0600

    learing method works

---
+++ LearingCSharp/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearingCSharp
+{
+    class Methods
+    {
+        static void Main(string[] args)
+        {
+            ReverseString();
+
+
+            Console.ReadLine();
+        }
+
+        private static void ReverseString()
+        {
+            string message = "Hello world!";
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            foreach (var item in messageArray)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
