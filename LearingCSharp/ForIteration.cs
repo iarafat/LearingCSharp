38a03de9224d34c785b9cbfad26a00aa67754347
iarafat <miyasinarafat@gmail.com>
2019-03-26 10:53:15 +0600

    init commit

---
+++ LearingCSharp/ForIteration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearingCSharp
+{
+    class ForIteration
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven");
+                    break;
+                }
+            }
+
+            for (int myValuei = 0; myValuei < 12; myValuei++)
+            {
+                Console.WriteLine(myValuei);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LearingCSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearingCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x;
+            //int y;
+
+            //x = 7;
+            //y = x + 3;
+            //Console.WriteLine(y);
+
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+
+            //Console.Write("Type your last name: ");
+            //string myLastName;
+            //myLastName = Console.ReadLine();
+
+            Console.Write("Type your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+
+            //int x = 44;
+            //string y = "Bob";
+            //int z = x + y;
+            //Console.WriteLine(z);
+        }
+    }
+}
