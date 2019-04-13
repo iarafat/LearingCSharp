0ee4296b4a4edd213e39cba72f4e6a848516d101
iarafat <miyasinarafat@gmail.com>
2019-04-13 11:55:43 +0600

    completed class 17

---
+++ LearingCSharp/AssembliesAndNamespaces.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace LearingCSharp
+{
+    class AssembliesAndNamespaces
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+            File.WriteAllText(@"C:\Users\Arafat\source\repos\LearingCSharp\LearingCSharp\AssembliesAndNamespaces.txt", reply);
+            Console.ReadLine();
+        }
+    }
+}
