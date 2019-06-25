ebc304107b7cfcd39d71d72a9635b805c246914a
iarafat <miyasinarafat@gmail.com>
2019-06-25 08:20:03 +0600

    added entity framework and sql client

---
+++ MyClient/MyClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MyCodeLibrary;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // @"C:\Users\Arafat\source\repos\LearingCSharp\MyCodeLibrary\ScrapeWebpage.txt"
+            Scrape myScrape = new Scrape();
+
+            string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com");
+            string value2 = myScrape.ScrapeWebpage("http://msdn.microsoft.com");
+
+            Console.WriteLine(value);
+        }
+    }
+}
